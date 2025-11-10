6e9cf4fbd4a08de20c22a0f8fdb5ef0f2c909efc
Samuel <67723030+ssimiao@users.noreply.github.com>
2025-11-10 19:38:54 -0300

    build: Add Maven publishing configuration to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,37 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     kotlin("jvm") version "2.2.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0"
+    id("maven-publish")
 }
 
 group = "io.magesoftware"
 version = "0.0.1"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "io.magesoftware"
+            artifactId = "ktoon"
+            version = "0.0.1"
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ssimiao/abacatepay-kotlin-sdk")
+            credentials {
+                // Ensure environment variables are correctly accessed
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
